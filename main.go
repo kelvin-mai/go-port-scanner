057bd16dac995d30deef7ae1fb567c8ec9b2df35
Kelvin Mai <kelvin.mai002@gmail.com>
2020-08-06 21:22:55 -0700

    completed assignment

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kelvin-mai/go-port-scanner/port"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	// called with url
+	scanned, ok := port.GetOpenPorts("www.freecodecamp.com", port.PortRange{Start: 75, End: 85})
+	if ok {
+		port.DisplayScanResult(scanned)
+	}
 
-	open := port.ScanPort("tcp", "localhost", 1313)
-	fmt.Printf("Port Open: %t\n", open)
+	// called with ip address
+	scanned, ok = port.GetOpenPorts("104.26.10.78", port.PortRange{Start: 8079, End: 8090})
+	if ok {
+		port.DisplayScanResult(scanned)
+	}
 
-	results := port.InitialScan("localhost")
-	fmt.Println(results)
+	// verbose called with ip address and no host name returned -- single open port
+	scanned, ok = port.GetOpenPorts("104.26.10.78", port.PortRange{Start: 440, End: 450})
+	if ok {
+		port.DisplayScanResult(scanned)
+	}
+
+	// verbose called with ip adress and valid host name returned -- single open port
+	scanned, ok = port.GetOpenPorts("137.74.187.104", port.PortRange{Start: 440, End: 450})
+	if ok {
+		port.DisplayScanResult(scanned)
+	}
+
+	// verbose called with host name -- multiple ports returned
+	scanned, ok = port.GetOpenPorts("scanme.nmap.org", port.PortRange{Start: 20, End: 80})
+	if ok {
+		port.DisplayScanResult(scanned)
+	}
 }
--- port/port.go
+++ port/port.go
@@ -1,36 +1,104 @@
 package port
 
 import (
+	"fmt"
 	"net"
 	"strconv"
 	"time"
 )
 
-type ScanResult struct {
-	Port  string
-	State string
+type PortResult struct {
+	Port    int
+	State   bool
+	Service string
 }
 
-func ScanPort(protocol, hostname string, port int) ScanResult {
-	result := ScanResult{Port: protocol + "/" + strconv.Itoa(port)}
+type PortRange struct {
+	Start, End int
+}
+
+type ScanResult struct {
+	hostname string
+	ip       []net.IP
+	results  []PortResult
+}
+
+var common = map[int]string{
+	7:    "echo",
+	20:   "ftp",
+	21:   "ftp",
+	22:   "ssh",
+	23:   "telnet",
+	25:   "smtp",
+	43:   "whois",
+	53:   "dns",
+	67:   "dhcp",
+	68:   "dhcp",
+	80:   "http",
+	110:  "pop3",
+	123:  "ntp",
+	137:  "netbios",
+	138:  "netbios",
+	139:  "netbios",
+	143:  "imap4",
+	443:  "https",
+	513:  "rlogin",
+	540:  "uucp",
+	554:  "rtsp",
+	587:  "smtp",
+	873:  "rsync",
+	902:  "vmware",
+	989:  "ftps",
+	990:  "ftps",
+	1194: "openvpn",
+	3306: "mysql",
+	5000: "unpn",
+	8080: "https-proxy",
+	8443: "https-alt",
+}
+
+func ScanPort(protocol, hostname string, port int) PortResult {
+	result := PortResult{Port: port}
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
-
 	if err != nil {
-		result.State = "Closed"
+		result.State = false
 		return result
 	}
 	defer conn.Close()
 
-	result.State = "Opened"
+	result.State = true
 	return result
 }
 
-func InitialScan(hostname string) []ScanResult {
-	var results []ScanResult
-	for i := 1; i <= 1024; i++ {
-		results = append(results, ScanPort("tcp", hostname, i))
-		results = append(results, ScanPort("udp", hostname, i))
+func GetOpenPorts(hostname string, ports PortRange) (ScanResult, bool) {
+	var results []PortResult
+	var scanned ScanResult
+	addr, err := net.LookupIP(hostname)
+	if err != nil {
+		return scanned, false
 	}
-	return results
+	for i := ports.Start; i <= ports.End; i++ {
+		if v, ok := common[i]; ok {
+			result := ScanPort("tcp", hostname, i)
+			result.Service = v
+			results = append(results, result)
+		}
+	}
+	scanned = ScanResult{
+		hostname: hostname,
+		ip:       addr,
+		results:  results,
+	}
+	return scanned, true
+}
+
+func DisplayScanResult(result ScanResult) {
+	ip := result.ip[len(result.ip)-1]
+	fmt.Printf("Open ports for %s (%s)\n", result.hostname, ip.String())
+	for _, v := range result.results {
+		if v.State {
+			fmt.Printf("%d	%s\n", v.Port, v.Service)
+		}
+	}
 }
